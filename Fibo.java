c6705d193155010e97bd9323d82ec9e8573f6885
WeronikaWisz <32033773+WeronikaWisz@users.noreply.github.com>
2019-10-14 23:23:32 +0200

    Create Fibo.java

---
+++ Fibo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        if(n<1 || n>45){
+            return;
+        }
+
+        int[] tab = new int[n];
+        tab[0] = 0;
+        tab[1] = 1;
+
+        for(int i = 2; i < n; i++){
+            tab[i] = tab[i-1] + tab[i-2];
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(tab[i]);
+        }
+
+    }
+}
